0f2a4eb25ea8566ce51c3b5d27ff7a6b1fa2b1e6
Chester <chesterdeveloper2000@gmail.com>
2020-08-13 18:33:24 +0800

    navbar for mobile finalized (I hope)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,57 @@
-const menuBars = document.getElementById("menu-bars");
-menuBars.addEventListener("click", () => {
-    menuBars.classList.toggle("change");
+const hamburgerMenu = document.getElementById("hamburger");
+const mobileLi = document.querySelector(".collapsible");
+const overlay = document.querySelector(".overlay")
+const body = document.body
+
+// event listeners
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("change");
+
+    mobileLi.classList.toggle("collapsed");
+
+    overlay.classList.toggle("showOverlay")
+
+    body.classList.toggle("noScroll")
+
 });
+
+/* ==============================================================
+show navbar when scroll up, hide when scroll down
+*/
+const doc = document.documentElement;
+const w = window;
+
+let curScroll = w.scrollY || doc.scrollTop;
+let prevScroll = w.scrollY || doc.scrollTop;
+let curDirection = 0;
+let prevDirection = 0;
+
+const header = document.querySelector(".navbar");
+
+const checkScroll = () => {
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+        // scrolled down
+        curDirection = 2;
+    } else {
+        // scrolled up
+        curDirection = 1;
+    }
+
+    if (curDirection !== prevDirection) {
+        toggleHeader();
+    }
+    prevDirection = curDirection;
+    prevScroll = curScroll;
+};
+
+const toggleHeader = () => {
+    if (curDirection === 2) {
+        header.classList.add("hide");
+    } else if (curDirection === 1) {
+        header.classList.remove("hide");
+    }
+};
+
+window.addEventListener("scroll", checkScroll);
+//================================================================
