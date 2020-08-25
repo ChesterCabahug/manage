2a5f54d7070bb24f6ad40d8b494fd7eb28c8c88b
Chester <chesterdeveloper2000@gmail.com>
2020-08-25 20:05:34 +0800

    reformat: shorter code of hiding the nav bar

--- js/app.js
+++ js/app.js
@@ -17,51 +17,68 @@ hamburgerMenu.addEventListener("click", () => {
 /* ==============================================================
 show navbar when scroll up, hide when scroll down
 */
-const doc = document.documentElement;
-const w = window;
+// const doc = document.documentElement;
+// const w = window;
 
-let curScroll = w.scrollY || doc.scrollTop;
-let prevScroll = w.scrollY || doc.scrollTop;
-let curDirection = 0;
-let prevDirection = 0;
+// let curScroll = w.scrollY || doc.scrollTop;
+// let prevScroll = w.scrollY || doc.scrollTop;
+// let curDirection = 0;
+// let prevDirection = 0;
 
-const header = document.querySelector(".navbar");
+// const header = document.querySelector(".navbar");
 
-const threshold = 200;
-let toggled;
+// const threshold = 200;
+// let toggled;
 
-const checkScroll = () => {
-    curScroll = w.scrollY || doc.scrollTop;
-    if (curScroll > prevScroll) {
-        // scrolled down
-        curDirection = 2;
-    } else {
-        // scrolled up
-        curDirection = 1;
-    }
+// const checkScroll = () => {
+//     curScroll = w.scrollY || doc.scrollTop;
+//     if (curScroll > prevScroll) {
+//         // scrolled down
+//         curDirection = 2;
+//     } else {
+//         // scrolled up
+//         curDirection = 1;
+//     }
 
-    if (curDirection !== prevDirection) {
-        toggled = toggleHeader();
-    }
+//     if (curDirection !== prevDirection) {
+//         toggled = toggleHeader();
+//     }
 
-    if (toggled) {
-        prevDirection = curDirection;
-    }
+//     if (toggled) {
+//         prevDirection = curDirection;
+//     }
 
-    prevScroll = curScroll;
-};
+//     prevScroll = curScroll;
+// };
 
-const toggleHeader = () => {
-    toggled = true;
-    if (curDirection === 2 && curScroll > threshold) {
-        header.classList.add("hide");
-    } else if (curDirection === 1) {
-        header.classList.remove("hide");
-    } else {
-        toggled = false;
-    }
-    return toggled;
-};
+// const toggleHeader = () => {
+//     toggled = true;
+//     if (curDirection === 2 && curScroll > threshold) {
+//         header.classList.add("hide");
+//     } else if (curDirection === 1) {
+//         header.classList.remove("hide");
+//     } else {
+//         toggled = false;
+//     }
+//     return toggled;
+// };
 
-window.addEventListener("scroll", checkScroll);
+// window.addEventListener("scroll", checkScroll);
 //================================================================
+
+
+
+// shorter version:
+// show navbar when scroll up, hide when scroll down
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos) {
+        document.querySelector(".navbar").style.top = "0";
+    } else {
+        document.querySelector(".navbar").style.top = "-130px";
+    }
+
+    prevScrollpos = currentScrollpos;
+};
